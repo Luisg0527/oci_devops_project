af900080678cc8b3920526c9b0a00f6ff2581b7d
LuisG0527 <luisgarzag27@gmail.com>
2026-04-23 12:19:50 -0600

    FIX: another route-fixing try

--- MtdrSpring/backend/src/main/java/com/ociproject/config/ApiPathConfig.java
+++ MtdrSpring/backend/src/main/java/com/ociproject/config/ApiPathConfig.java
@@ -1,6 +1,7 @@
 package com.ociproject.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,6 +11,10 @@ public class ApiPathConfig implements WebMvcConfigurer {
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
-        configurer.addPathPrefix("/api/v1", HandlerTypePredicate.forBasePackage("com.ociproject.controller"));
+        configurer.addPathPrefix(
+                "/api/v1",
+                HandlerTypePredicate.forBasePackage("com.ociproject.controller")
+                        .and(HandlerTypePredicate.forAnnotation(RestController.class))
+        );
     }
 }
--- MtdrSpring/backend/src/main/java/com/ociproject/web/SpaForwardController.java
+++ MtdrSpring/backend/src/main/java/com/ociproject/web/SpaForwardController.java
@@ -1,4 +1,4 @@
-package com.ociproject.controller;
+package com.ociproject.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
